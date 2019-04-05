24330b72ebc61a93bb2a9edb3ec1bfc4688ce9c4
yutaka0m <yutaka.miyamae@askul.com>
2019-04-05 13:40:43 +0900

    Practice012の解答

--- src/main/java/practices/Practice012.java
+++ src/main/java/practices/Practice012.java
@@ -1,5 +1,7 @@
 package practices;
 
+import java.util.Arrays;
+
 /**
  * 配列の中身を小さい順に並べ替える関数 sort を作成しなさい
  *
@@ -19,8 +21,7 @@ public class Practice012 {
 
     // 並べ替えられた配列を返す
     public static int[] sort(int[] n) {
-
+        Arrays.sort(n);
         return n;
     }
-
 }
