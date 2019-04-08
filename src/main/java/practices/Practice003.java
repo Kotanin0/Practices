c72a1250009ae5985a279872d4fbc9f305a98a4e
tao-ki <taoki@askul.com>
2019-04-08 14:16:12 +0900

    Practice003.実施

--- src/main/java/practices/Practice003.java
+++ src/main/java/practices/Practice003.java
@@ -5,10 +5,10 @@ package practices;
  *
  * 引数：整数
  * 返却：以下の4つの文字列のいずれか
- * 正の偶数
- * 正の奇数
- * 負の偶数
- * 負の奇数
+ * - 正の偶数
+ * - 正の奇数
+ * - 負の偶数
+ * - 負の奇数
  *
  * ※ 負の数であっても、2 で割り切れれば偶数、そうでなければ奇数とする。
  * ※ 0 は 正の偶数 であるとする。
@@ -22,8 +22,20 @@ public class Practice003 {
 
     // 正の偶数、正の奇数、負の偶数、負の奇数のいずれかを返却する
     public static String displayText(int n) {
+        if (n >= 0) {
+            if (n % 2 ==0) {
+                return "正の偶数";
+            } else {
+                return "正の奇数";
+            }
+        } else {
+            if (n % 2 ==0) {
+                return "負の偶数";
+            } else {
+                return "負の奇数";
+            }
+        }
 
-        return "";
     }
 
 }
