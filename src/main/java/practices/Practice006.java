77f9642e50e7cc602c908a14b90fce29780856a8
tao-ki <taoki@askul.com>
2019-04-09 16:52:17 +0900

    006解答 そもそも + と * を間違えました。。。 while文を使って書き直しました。

--- src/main/java/practices/Practice006.java
+++ src/main/java/practices/Practice006.java
@@ -2,10 +2,10 @@ package practices;
 
 /**
  * 九九の答えを表示する関数 showKuku を作成しなさい
- *
+ * <p>
  * 引数：整数
  * 返却：以下のように、数字を半角スペースで区切ったもの
- *
+ * <p>
  * 　例）引数を1にした場合の出力は「1 2 3 4 5 6 7 8 9」
  */
 
@@ -17,8 +17,19 @@ public class Practice006 {
 
     // 九九の答え
     public static String showKuku(int n) {
-
-        return n + 1 + " " + n + 2 + " " + n + 3 + " " + n + 4 + " " + n + 5 + " " + n + 6 + " " + n + 7 + " " + n + 8 + " " + n + 9;
+        if (n < 0) {
+            throw new IllegalArgumentException("");
+        } else {
+            String Kuku = "";
+            int i = 1;
+            while (i <= 9) {
+                Kuku += n * i;
+                i++;
+                if (i <= 9) {
+                    Kuku += " ";
+                }
+            }
+            return Kuku;
+        }
     }
-
 }
