0f302006e998545733c6326d172d5cde969dc0a8
keitaoku <keita.okuhira@askul.com>
2019-04-09 22:09:46 +0900

    Practice001の解答

--- src/main/java/practices/Practice006.java
+++ src/main/java/practices/Practice006.java
@@ -2,10 +2,10 @@ package practices;
 
 /**
  * 九九の答えを表示する関数 showKuku を作成しなさい
- *
+ * <p>
  * 引数：整数
  * 返却：以下のように、数字を半角スペースで区切ったもの
- *
+ * <p>
  * 　例）引数を1にした場合の出力は「1 2 3 4 5 6 7 8 9」
  */
 
@@ -17,8 +17,15 @@ public class Practice006 {
 
     // 九九の答え
     public static String showKuku(int n) {
-
-        return "";
+        int[] k = new int[9];
+        if (0 >= n || 10 < n) {
+            throw new IllegalArgumentException("割り切れない");
+        } else {
+            for (int i = 0; i < 9; i++) {
+                k[i] = n * (i + 1);
+            }
+            return k[0] + " " + k[1] + " " + k[2] + " " + k[3] + " " + k[4] + " " + k[5] + " " + k[6] + " " + k[7] + " " + k[8];
+        }
     }
 
 }
