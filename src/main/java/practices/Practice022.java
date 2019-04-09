9e5c872f217d543b9abe64b0510ed9c0891d2ec5
yutaka0m <yutaka.miyamae@askul.com>
2019-04-09 10:50:05 +0900

    022の解答

--- src/main/java/practices/Practice022.java
+++ src/main/java/practices/Practice022.java
@@ -1,5 +1,7 @@
 package practices;
 
+import java.util.Random;
+
 /**
  * 以前作成した素数判定関数を100000回実行したときの除算回数、処理時間をmsで出力しなさい
  *
@@ -8,8 +10,33 @@ package practices;
 public class Practice022 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
+        int countDivision = 0;
+        Random rand = new Random();
 
+        long start = System.currentTimeMillis();
+        for(int i = 0; i < 100000; i++) {
+            countDivision += isPrime(rand.nextInt(9999) + 1);
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("除算回数：" + countDivision + "回");
+        System.out.println("処理時間：" + (end - start)  + "ms");
     }
 
-
+    // 素数判定
+    public static int isPrime(int n) {
+        int countDivision = 0;
+        if(n < 1){
+            throw new IllegalArgumentException("引数の値が不正です");
+        }
+        if (n < 2) return 0;
+        for (int i = 2; i < n; i++) {
+            countDivision++;
+            if (n % i == 0)
+            {
+                return countDivision++;
+            }
+        }
+        return countDivision;
+    }
 }
