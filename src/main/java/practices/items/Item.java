40d0d3e4158e96beeb00d415df1a3f85acd0b9f3
yutaka0m <yutaka.miyamae@askul.com>
2019-04-09 13:50:42 +0900

    101の解答

--- src/main/java/practices/items/Item.java
+++ src/main/java/practices/items/Item.java
@@ -2,9 +2,29 @@ package practices.items;
 
 /**
  * 商品を管理するクラス Item
- *
  */
 
 
 public class Item {
+    private String productName;
+    private String productCatchCopy;
+    private String makerName;
+    private int price;
+    private int quantityOfStocks;
+
+    public Item(String productName, String productCatchCopy, String makerName, int price, int quantityOfStocks) {
+        this.productName = productName;
+        this.productCatchCopy = productCatchCopy;
+        this.makerName = makerName;
+        this.price = price;
+        this.quantityOfStocks = quantityOfStocks;
+    }
+
+    public String printInfo() {
+        return "商品名："          + this.productName      + " " +
+                "キャッチコピー：" + this.productCatchCopy + " " +
+                "メーカー名："     + this.makerName        + " " +
+                "価格："           + this.price            + " " +
+                "在庫数："           + this.quantityOfStocks;
+    }
 }
--- src/main/java/practices/items/Practice101.java
+++ src/main/java/practices/items/Practice101.java
@@ -2,18 +2,30 @@ package practices.items;
 
 /**
  * 商品を管理するクラスItemを作成しなさい
- *
+ * <p>
  * Itemは以下情報が格納でき、取り出しができること
- *
+ * <p>
  * - 商品名
  * - 商品キャッチコピー
  * - メーカー名
  * - 価格
  * - 在庫数
- *
+ * <p>
  * 作成したItemを利用して、商品情報を5つ登録し、それらを全て出力しなさい
- *
  */
 
 public class Practice101 {
+    public static void main(String[] args) {
+        Item[] item = {
+                new Item("サンダーバード１号", "超音速有人ロケット機", "国際救助隊", 100, 1),
+                new Item("サンダーバード２号", "大型輸送機", "国際救助隊", 200, 1),
+                new Item("サンダーバード３号", "再使用型宇宙往還機", "国際救助隊", 300, 1),
+                new Item("サンダーバード４号", "潜航艇", "国際救助隊", 400, 1),
+                new Item("サンダーバード５号", "有人宇宙ステーション", "国際救助隊", 500, 1),
+        };
+
+        for (Item x : item) {
+            System.out.println(x.printInfo());
+        }
+    }
 }
