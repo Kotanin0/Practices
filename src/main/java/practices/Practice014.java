c5bba3b611ca2df7c700c6c5aa65b66b5f68920a
kaitonton <kaito.suzuki@askul.com>
2019-04-10 18:10:11 +0900

    practice13,14完了です。（鈴木：2019/4/10）

--- src/main/java/practices/Practice014.java
+++ src/main/java/practices/Practice014.java
@@ -11,13 +11,25 @@ package practices;
 public class Practice014 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
+        int uru = 2000;
+        System.out.println(isLeapYear(uru));
 
     }
 
     // うるう年ならtrue
     public static boolean isLeapYear(int n) {
+// うるう年は2000年、2004年、…2020年　year+4
+// グレゴリオ暦では 100 で割り切れる年 (1900 年など) は、400で割り切れる場合のみうるう年。
+// そのため1700、1800、1900、2100、2200、2300、2500、2600はうるう年ではない。
 
-        return true;
+        if(n % 4 == 0){
+            if(n % 100 != 0 || n % 400 == 0){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
-
 }
