45e63d99470c6d8e9396b873160e564ed2895525
yama7 <h_yamana@askul.com>
2019-04-10 13:34:09 +0900

    Practice007の回答(山名）

--- src/main/java/practices/Practice007.java
+++ src/main/java/practices/Practice007.java
@@ -2,7 +2,7 @@ package practices;
 
 /**
  * 素数を判定する関数 isPrime を作成しなさい
- *
+ * <p>
  * 引数：任意の数値
  * 返却：true/false
  */
@@ -11,12 +11,26 @@ public class Practice007 {
 
     public static void main(String[] args) {
         // ここはご自由にお使いください
+        System.out.println(isPrime(5));
 
     }
+
 
     // 素数判定
     public static boolean isPrime(int n) {
+
+        if (n <= 0) {
+            throw new IllegalArgumentException("自然数で入力してください");
+        }
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+
+        }
         return true;
     }
-
 }
