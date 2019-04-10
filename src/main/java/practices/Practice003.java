bc6fe44e8474a93033f8e674710dbcd346a1f0b2
abi01120 <fumiaki.tsuchiya@askul.com>
2019-04-10 12:33:17 +0900

    土屋_Practice003の解答

--- src/main/java/practices/Practice003.java
+++ src/main/java/practices/Practice003.java
@@ -22,8 +22,11 @@ public class Practice003 {
 
     // 正の偶数、正の奇数、負の偶数、負の奇数のいずれかを返却する
     public static String displayText(int n) {
+        if(n >= 0) {
+            return n % 2 == 0 ? "正の偶数" : "正の奇数";
+        }else{
+            return n % 2 == 0 ? "負の偶数" : "負の奇数";
 
-        return "";
-    }
-
+            }
+        }
 }
