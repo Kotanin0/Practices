a7cd7d5caffc8d54195d3a97e474cc96e732133c
keitaoku <keita.okuhira@askul.com>
2019-04-10 12:28:04 +0900

    Practice001の解答

--- src/main/java/practices/Practice001.java
+++ src/main/java/practices/Practice001.java
@@ -2,20 +2,20 @@ package practices;
 
 /**
  * int 型の変数 x、y にそれぞれ数値を入力し
- *
+ * <p>
  * - 和
  * - 差（x－y）
  * - 積
  * - 商
  * - 余り(x÷y)
- *
+ * <p>
  * を求める関数をそれぞれ作成しなさい
  */
 
 public class Practice001 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-  }
+    }
 
     // 和
     public static int wa(int x, int y) {
@@ -34,14 +34,11 @@ public class Practice001 {
 
     // 商
     public static int sho(int x, int y) {
-        if( y == 0) { return 0;
-        } else {return x/y;}
+        return y == 0 ? 0 : x / y;
     }
 
     // 余り
-    public static int amari(int x, int y){
-     if ( y == 0) { return  0;
-     } else { return x % y ;
-    }
+    public static int amari(int x, int y) {
+        return y == 0 ? 0 : x % y;
     }
 }
--- src/main/java/practices/Practice005.java
+++ src/main/java/practices/Practice005.java
@@ -3,9 +3,9 @@ package practices;
 /**
  * 試験の点数を入力し、対応する結果を表示する関数 showResult を作成しなさい
  * 試験の仕様は以下とする
- *
+ * <p>
  * 引数：整数
- *
+ * <p>
  * 返却：以下の文字列のいずれか
  * 　80点以上：優
  * 　70-79点：良
@@ -21,18 +21,11 @@ public class Practice005 {
 
     // 試験の結果を返却する
     public static String showResult(int n) {
-        if (n >= 80) {
-            return "優";
-        }
-        else if( n >= 70){
-            return "良";
-        }
-        else if (n >= 60){
-            return "可";
-        }
-        else {
-            return "不可";
+        if (n >= 80) return "優";
+        if (n >= 70) return "良";
+        if (n >= 60) return "可";
+        else return "不可";
         }
     }
 
-}
+
