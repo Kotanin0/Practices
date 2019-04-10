1518634d3447aa04c519cb27139e21e967046ea6
yama7 <h_yamana@askul.com>
2019-04-10 18:48:02 +0900

    Practice002の回答

--- src/main/java/practices/Practice006.java
+++ src/main/java/practices/Practice006.java
@@ -2,41 +2,36 @@ package practices;
 
 /**
  * 九九の答えを表示する関数 showKuku を作成しなさい
- *
+ * <p>
  * 引数：整数
  * 返却：以下のように、数字を半角スペースで区切ったもの
- *
+ * <p>
  * 　例）引数を1にした場合の出力は「1 2 3 4 5 6 7 8 9」
  */
 
 public class Practice006 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-
+        System.out.println(showKuku(5));
     }
 
     // 九九の答え
     public static String showKuku(int n) {
-        if (n == 1) {
-            return ("1 2 3 4 5 6 7 8 9");
-        } else if (n == 2) {
-            return ("2 4 6 8 10 12 14 16 18");
-        } else if (n == 3) {
-            return ("3 6 9 12 15 18 21 24 27");
-        } else if (n == 4) {
-            return ("4 8 12 16 20 24 28 32 36");
-        } else if (n == 5) {
-            return ("5 10 15 20 25 30 35 40 45");
-        } else if (n == 6) {
-            return ("6 12 18 24 30 36 42 48 54");
-        } else if (n == 7) {
-            return ("7 14 21 28 35 42 49 56 63");
-        } else if (n == 8) {
-            return ("8 16 24 32 40 48 56 64 72");
-        } else if (n == 9) {
-            return ("9 18 27 36 45 54 63 72 81");
-        } else {
-            return (" ");
+        if (n <= 0) {
+            throw new IllegalArgumentException("自然数で入力してください");
         }
+        if (n >= 10) {
+            throw new IllegalArgumentException("自然数で入力してください");
+        }
+        //空の変数を定義
+        String kakeru = "";
+
+        //　1～９の間でｎに乗じていく、結果を変数kakeruに代入していく
+        for (int i = 1; i < 10; i++) {
+            kakeru += n * i + " ";
+        }
+        // 結果を返す、最後の空白を消す
+        return kakeru.trim();
     }
 }
+
