6a00b3b5043a8cc2630d8c6750fe0941ed1fe6ac
takeuchi <ryuta.takeuchi@askul.com>
2019-04-10 18:03:16 +0900

    005回答です

--- src/main/java/practices/Practice005.java
+++ src/main/java/practices/Practice005.java
@@ -3,9 +3,9 @@ package practices;
 /**
  * 試験の点数を入力し、対応する結果を表示する関数 showResult を作成しなさい
  * 試験の仕様は以下とする
- *
+ * <p>
  * 引数：整数
- *
+ * <p>
  * 返却：以下の文字列のいずれか
  * 　80点以上：優
  * 　70-79点：良
@@ -21,8 +21,15 @@ public class Practice005 {
 
     // 試験の結果を返却する
     public static String showResult(int n) {
-
-        return "";
+        if (n >= 80) {
+            return "優";
+        } else if ((80 > n) && (n >= 70)) {
+            return "良";
+        } else if ((70 > n) && (n >= 60)){
+            return "可";
+        } else{
+            return "不可";
+        }
     }
 
 }
