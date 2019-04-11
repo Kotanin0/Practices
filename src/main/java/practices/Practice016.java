4a9ed58bcba1c31c3dc44d23fce5efae643fab1c
kaitonton <kaito.suzuki@askul.com>
2019-04-11 09:50:02 +0900

    practice16完了です。（鈴木：2019/4/11）

--- src/main/java/practices/Practice016.java
+++ src/main/java/practices/Practice016.java
@@ -1,4 +1,5 @@
 package practices;
+import java.lang.Math;
 
 /**
  * 3の倍数と、3の付く数字のときに "fizz" と返却する関数 fizz を作成しなさい
@@ -11,12 +12,15 @@ package practices;
 public class Practice016 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-
+        int test = -13;
+        System.out.println(fizz(test));
     }
 
     // 3の倍数と、3の付く数字なら"fizz"
     public static String fizz(int n) {
 
+        if(n != 0 && n % 3 == 0){ return "fizz";}
+        if(Math.abs(n) % 10 == 3){ return "fizz";} //2つ条件書くと長い気がしたのでabs(絶対値計算)メソッド利用
         return "";
     }
 
