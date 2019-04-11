e0c532fc4dc8f62453f10bbb5b49ee3b6bfc2fdf
takeuchi <ryuta.takeuchi@askul.com>
2019-04-11 13:36:10 +0900

    007回答です

--- src/main/java/practices/Practice007.java
+++ src/main/java/practices/Practice007.java
@@ -11,12 +11,26 @@ public class Practice007 {
 
     public static void main(String[] args) {
         // ここはご自由にお使いください
+       System.out.println(isPrime(1));
 
     }
 
     // 素数判定
     public static boolean isPrime(int n) {
-        return true;
-    }
 
+        if((n == 0 )||(n == -1)) {
+            throw new IllegalArgumentException("引数の値が不正です");
+        }
+
+        if(n == 1){
+            return false; }
+        else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
