f969710fc9903a1ac53e5af8e170d5aefc82be19
Sakamune0 <junji.sakamune@askul.com>
2019-04-11 12:43:49 +0900

    変数名変更してみました。 キャメルケースも意識。

--- src/main/java/practices/Practice011.java
+++ src/main/java/practices/Practice011.java
@@ -4,37 +4,36 @@ import java.util.ArrayList;
 
 /**
  * 配列の中身を奇数と偶数に分類する関数 getOdd, getEven を作成しなさい
- *
+ * <p>
  * getOdd
  * 引数：整数の配列
  * 返却：奇数のみの配列
- *
+ * <p>
  * 　例）引数：[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
  * 　　　返却：[1, 3, 5, 7, 9]
- *
+ * <p>
  * getEven
  * 引数：整数の配列
  * 返却：偶数のみの配列
- *
+ * <p>
  * 　例）引数：[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
  * 　　　返却：[0, 2, 4, 6, 8]
- *
  */
 
 public class Practice011 {
     public static void main(String[] args) {
         // ここはご自由にお使いくだい
-        int[] TestArray1_arg = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] numberArrayList = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         int data[];
 
-        data = getOdd(TestArray1_arg); //なんの変数を渡すかを決める。()は。
+        data = getOdd(numberArrayList); //なんの変数を渡すかを決める。()は。
         for (int i = 0; i < data.length; i++) {
             System.out.println(data[i]);
         }
     }
 
     //()内を要求している。　　今回はint型の配列を返すといっている。
-    public static int[] ArrayLst_toArray(ArrayList<Integer> X) {
+    public static int[] getArrayList(ArrayList<Integer> X) {
         int[] returnList = new int[X.size()];
 
         for (int i = 0; i < X.size(); i++) {
@@ -58,7 +57,7 @@ public class Practice011 {
         }
         //返す時にint型に変更しないといけない。
         // ArrayLst_toArrayメソッドに配列arrayOddを渡す
-        int[] X = ArrayLst_toArray(arrayOdd);
+        int[] X = getArrayList(arrayOdd);
         return X;
 
     }
@@ -67,14 +66,14 @@ public class Practice011 {
     public static int[] getEven(int[] n) {
         ArrayList<Integer> arrayEven = new ArrayList<Integer>();
         int i;
-        int j=0;
-        for(i=0;i < n.length;i++){
-            if(n[i] % 2 == 0){
+        int j = 0;
+        for (i = 0; i < n.length; i++) {
+            if (n[i] % 2 == 0) {
                 arrayEven.add(n[i]);
                 j++;
             }
         }
-        return ArrayLst_toArray(arrayEven);
+        return getArrayList(arrayEven);
     }
 
 }
