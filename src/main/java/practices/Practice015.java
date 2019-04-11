c3186a578011c3b8642a8ec6c0fb17d636f3adf0
Sakamune0 <junji.sakamune@askul.com>
2019-04-11 12:44:39 +0900

    ifを使わず、3項演算子をもちいました。

--- src/main/java/practices/Practice015.java
+++ src/main/java/practices/Practice015.java
@@ -2,25 +2,19 @@ package practices;
 
 /**
  * 入力された文字の中に "ge" という文字列が含まれているか判定する関数 ge を作成しなさい
- *
+ * <p>
  * 引数：文字列
  * 返却：true/false
- *
  */
 
 public class Practice015 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
+        System.out.println(ge("genki"));
     }
 
     // "ge" という文字列が含まれているならtrue
     public static boolean ge(String str) {
-        if(str.contains("ge")){
-            return true;
-        }else{
-            return false;
-        }
-
+        return str.contains("ge") ? true : false;
     }
-
 }
