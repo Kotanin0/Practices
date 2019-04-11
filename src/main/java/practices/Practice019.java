35478b6253c9a2410f200bdae6014d7f2cfa0681
m4K1 <maki.tokumura@askul.com>
2019-04-11 19:19:51 +0900

    021できた

--- src/main/java/practices/Practice019.java
+++ src/main/java/practices/Practice019.java
@@ -4,7 +4,7 @@ package practices;
  * 入力された整数を 日本円（小銭のみ）で支払ったときの硬貨の枚数を出力する関数
  * coincheck
  * を作成しなさい
- *
+ * <p>
  * 例）
  * 入力：898
  * 出力：
@@ -14,7 +14,6 @@ package practices;
  * 10円＝4枚
  * 5円＝1枚
  * 1円＝3枚
- *
  */
 
 public class Practice019 {
@@ -25,20 +24,20 @@ public class Practice019 {
 
     public static String coincheck(int n) {
         // 引数が0以下の場合は空文字
-        if(n < 1) return "";
+        if (n < 1) return "";
 
         // 固定値の定義
         String br = System.getProperty("line.separator");
 
         //コインチェック
         String resultMessage = "";
-        for(Coin coin : Coin.values()){
-          int count = 0;
-          while(n - coin.getAmount() >= 0){
-              count++;
-              n -= coin.getAmount();
-          }
-          resultMessage += coin.getLabel() + "＝" + count + "枚" + br;
+        for (Coin coin : Coin.values()) {
+            int count = 0;
+            while (n - coin.getAmount() >= 0) {
+                count++;
+                n -= coin.getAmount();
+            }
+            resultMessage += coin.getLabel() + "＝" + count + "枚" + br;
         }
 
         return resultMessage.trim();
@@ -64,10 +63,11 @@ public class Practice019 {
         }
 
         // げったー
-        public int getAmount(){
+        public int getAmount() {
             return this.amount;
         }
-        public String getLabel(){
+
+        public String getLabel() {
             return this.label;
         }
     }
