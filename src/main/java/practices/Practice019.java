1cc32a2525c3f56337ce6f66be08e18e5e642cd7
tao-ki <taoki@askul.com>
2019-04-11 16:51:00 +0900

    018 Coin-check 青木

--- src/main/java/practices/Practice019.java
+++ src/main/java/practices/Practice019.java
@@ -4,7 +4,7 @@ package practices;
  * 入力された整数を 日本円（小銭のみ）で支払ったときの硬貨の枚数を出力する関数
  * coincheck
  * を作成しなさい
- *
+ * <p>
  * 例）
  * 入力：898
  * 出力：
@@ -14,18 +14,31 @@ package practices;
  * 10円＝4枚
  * 5円＝1枚
  * 1円＝3枚
- *
  */
 
 public class Practice019 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-
+//        coincheck(890);
     }
 
     public static String coincheck(int n) {
+        if (n < 1) return "";
+        String br = System.getProperty("line.separator"); // 改行コード指定
+        String result = "";
+        int otsuri = n;
+        int[] coin = {500, 100, 50, 10, 5, 1};
+        for (int i = 0; i < coin.length; i++) {
+            result += coin[i] + "円＝" + (otsuri / coin[i]) + "枚";
+            // 最後（1円のとき）だけ処理しない部分
+            if (coin.length != i + 1) {
+                result += br;
+                otsuri = otsuri % coin[i]; //coin円使った後のおつり
+            }
+        }
+        System.out.println(result);
 
-        return "";
+        return result;
     }
 
 }
