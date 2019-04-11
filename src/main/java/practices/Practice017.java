786552e415d3b2ccf9ba1ab903af32f88d274e50
kaitonton <kaito.suzuki@askul.com>
2019-04-11 12:37:55 +0900

    practice17修正完了です。（鈴木：2019/4/11）

--- src/main/java/practices/Practice017.java
+++ src/main/java/practices/Practice017.java
@@ -11,17 +11,16 @@ package practices;
 public class Practice017 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-        int test = 15;
+        int test = 51;
         System.out.println(buzz(test));
     }
 
     // 5の倍数と、5の付く数字なら"buzz"
     public static String buzz(int n) {
+        String str = String.valueOf(n);
         if(n != 0 && n % 5 == 0){ return "buzz";}
-        // 5の付く整数＝5の倍数のため不要
-        // if(Math.abs(n) % 10 == 5){ return "buzz";} //2つ条件書くと長い気がしたのでabs(絶対値計算)メソッド利用
+        if(str.contains("5")){ return "buzz";}
         return "";
-
     }
 
 }
