7b6e08e4a94616d3d8f32fbd6bac37d97c6ba17f
m4K1 <maki.tokumura@askul.com>
2019-04-11 18:01:35 +0900

    019解いた

--- src/main/java/practices/Practice019.java
+++ src/main/java/practices/Practice019.java
@@ -19,13 +19,51 @@ package practices;
 
 public class Practice019 {
     public static void main(String[] args) {
-        // ここはご自由にお使いください
+        System.out.println(coincheck(898));
 
     }
 
     public static String coincheck(int n) {
+        // 引数が0以下の場合は空文字
+        if(n < 1) return "";
 
-        return "";
+        // 固定値の定義
+        String br = System.getProperty("line.separator");
+
+        //コインチェック
+        String resultMessage = "";
+        for(Coin coin : Coin.values()){
+          int count = 0;
+          while(n - coin.getAmount() >= 0){
+              count++;
+              n -= coin.getAmount();
+          }
+          resultMessage += coin.getAmount() + "円＝" + count + "枚" + br;
+        }
+
+        return resultMessage.trim();
     }
 
+    // コインのEnum（Enumを使いたかったという気持ち）
+    protected enum Coin {
+        fiveHundred(500),
+        hundred(100),
+        fifty(50),
+        ten(10),
+        five(5),
+        one(1);
+
+        // フィールド定義
+        private int amount;
+
+        // コンストラクタ定義
+        Coin(int amount) {
+            this.amount = amount;
+        }
+
+        // げったー
+        public int getAmount(){
+            return this.amount;
+        }
+    }
 }
