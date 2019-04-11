2fb96dd54cb7a94c752dbb65a3e265408f446946
kaitonton <kaito.suzuki@askul.com>
2019-04-11 10:24:12 +0900

    practice16修正完了です。（鈴木：2019/4/11）

--- src/main/java/practices/Practice016.java
+++ src/main/java/practices/Practice016.java
@@ -1,4 +1,6 @@
 package practices;
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+
 import java.lang.Math;
 
 /**
@@ -12,16 +14,27 @@ import java.lang.Math;
 public class Practice016 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-        int test = -13;
+        int test = 3100000;
         System.out.println(fizz(test));
     }
 
     // 3の倍数と、3の付く数字なら"fizz"
     public static String fizz(int n) {
+//文字列変換ケース
 
+        String str = String.valueOf(n);
         if(n != 0 && n % 3 == 0){ return "fizz";}
-        if(Math.abs(n) % 10 == 3){ return "fizz";} //2つ条件書くと長い気がしたのでabs(絶対値計算)メソッド利用
+        if(str.contains("3")){ return "fizz";}
         return "";
+
+//整数のままケース　for文でくくったとしても10^n桁が増えるとがきりがない
+/*
+        int a = Math.abs(n);
+        if(a != 0 && a % 3 == 0){ return "fizz";}
+        if(a % 10 == 3){ return "fizz";} //2つ条件書くと長い気がしたのでabs(絶対値計算)メソッド利用
+        if(a / 10 == 3 || a /100 == 3 || a / 1000 == 3 || a / 10000 == 3){return "fizz";}
+        return "";
+*/
     }
 
 }
