efbbc4579479e1ddab148a9cc1adde7916e81995
Sakamune0 <junji.sakamune@askul.com>
2019-04-11 13:18:33 +0900

    問18　ガード節・practice16,17を用いて作成。

--- src/main/java/practices/Practice018.java
+++ src/main/java/practices/Practice018.java
@@ -5,10 +5,9 @@ package practices;
  * 5の倍数と、5の付く数字のときに "buzz" と返却
  * どちらにもあてはまるときに "fizzbuzz" と返却
  * する関数 fizzbuzz を作成しなさい
- *
+ * <p>
  * 引数：整数
  * 返却："fizz" / "buzz" / "fizzbuzz" / "" (あてはまらない場合は空文字)
- *
  */
 
 public class Practice018 {
@@ -18,17 +17,16 @@ public class Practice018 {
     }
 
     public static String fizzbuzz(int n) {
-        String strFizzBuzz = String.valueOf(n);
-        if (n == 0) {
-            return "";
-        }else if((n % 5 == 0 || strFizzBuzz.contains("5")) && (n % 3 == 0 || strFizzBuzz.contains("3"))){
-            return "fizzbuzz";
-        }else if(n % 5 == 0 || strFizzBuzz.contains("5")){
-            return "buzz";
-        }else if(n % 3 == 0 || strFizzBuzz.contains("3")){
-            return "fizz";
-        }
-            return "";
-    }
+        //ガード節使用。
+        String fizz = Practice016.fizz(n);
+        String buzz = Practice017.buzz(n);
 
+        if (fizz == "fizz" && buzz == "buzz") return "fizzbuzz";
+        if (fizz == "fizz") return "fizz";
+        if (buzz == "buzz") return "buzz";
+        return "";
+
+    }
 }
+
+
