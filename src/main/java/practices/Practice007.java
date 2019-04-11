69eb05a4faf5d85e37128b06d7068fd0cb4afa0c
m4K1 <maki.tokumura@askul.com>
2019-04-11 10:58:03 +0900

    007~008と002の指摘事項修正

--- src/main/java/practices/Practice007.java
+++ src/main/java/practices/Practice007.java
@@ -1,8 +1,10 @@
 package practices;
 
+import java.math.BigDecimal;
+
 /**
  * 素数を判定する関数 isPrime を作成しなさい
- *
+ * <p>
  * 引数：任意の数値
  * 返却：true/false
  */
@@ -10,12 +12,25 @@ package practices;
 public class Practice007 {
 
     public static void main(String[] args) {
-        // ここはご自由にお使いください
+        System.out.println(isPrime(2));
 
     }
 
     // 素数判定
     public static boolean isPrime(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        }
+        // 2は素数
+        if(n == 2) return true;
+        // 1と偶数は素数じゃない
+        if (n == 1 || n % 2 == 0) return false;
+
+        for (int i = 2; i <= n-1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 
--- src/main/java/practices/Practice008.java
+++ src/main/java/practices/Practice008.java
@@ -1,5 +1,7 @@
 package practices;
 
+import java.util.ArrayList;
+
 /**
  * 2以上の数値を入力し、素因数分解した結果を表示しなさい。
  *
@@ -12,14 +14,49 @@ package practices;
 
 public class Practice008 {
     public static void main(String[] args) {
-        // ここはご自由にお使いください
+        System.out.println(factorization(1));
 
     }
 
     // 素因数分解
     public static String factorization(int n) {
+        validator(n);
 
-        return "";
+        Integer primes[]= getPrimeList(n);
+
+        String result = "";
+        for(int i = 0; i < primes.length; i++){
+            while(n % primes[i] == 0){
+                result += primes[i] + " ";
+                n = n / primes[i];
+            }
+        }
+
+        return result.trim();
     }
 
+    //素因数分解に必要な、素数の配列を返却する
+    public static Integer[] getPrimeList(int n){
+        //インスタンス
+        Practice007 practice007 = new Practice007();
+
+        //素数判定
+        ArrayList<Integer> primeList = new ArrayList<Integer>();
+        for(int i = 2; i <= n; i++){
+            if(practice007.isPrime(i)){
+                primeList.add(i);
+            }
+        }
+
+        //Listを配列に変換
+        Integer resultList[] = primeList.toArray(new Integer[primeList.size()]);
+        return resultList;
+    }
+
+    //不正な数で例外をスロー
+    public static void validator(int n){
+        if(n < 2){
+            throw new IllegalArgumentException("不正な数字です");
+        }
+    }
 }
