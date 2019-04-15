ad6165966ab0a17fbb9ded347324352316227930
kaitonton <kaito.suzuki@askul.com>
2019-04-15 14:29:11 +0900

    コタモン25完了です。※24も"contains"→"endWith"に変更しました（4/15鈴木）

--- src/main/java/practices/Practice024.java
+++ src/main/java/practices/Practice024.java
@@ -20,10 +20,10 @@ public class Practice024 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
         //都道府県を格納
-        ArrayList<String> prefecturesList = new ArrayList<String>(Arrays.asList("北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"));
+        ArrayList<String> prefecturesList = new ArrayList<>(Arrays.asList("北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"));
         //都道府県チェックメソッド呼び出し
 
-        String order = "府";
+        String order = "都";
 
         System.out.println("戻り値確認："+checkPre(prefecturesList, order));
 
@@ -34,10 +34,10 @@ public class Practice024 {
     public static List<String> checkPre(List<String> preList, String order){
 //引数確認
 //      System.out.println(preList);
-        ArrayList<String> check = new ArrayList<String>();
+        ArrayList<String> check = new ArrayList<>();
 //引数の文字列「都道府県」を基に返却するリストのみを作成するパターン
         for(int i = 0; i< preList.size();i++){
-            if(preList.get(i).contains(order)) {
+            if(preList.get(i).endsWith(order)) {
                 check.add(preList.get(i));
             }
         }
@@ -51,11 +51,11 @@ public class Practice024 {
         ArrayList<String> dou = new ArrayList<String>();
         ArrayList<String> to = new ArrayList<String>();
         for(int i = 0; i< preList.size();i++){
-            if(preList.get(i).contains("県")) {
+            if(preList.get(i).endWith("県")) {
                 ken.add(preList.get(i));
-            }else if(preList.get(i).contains("府")){
+            }else if(preList.get(i).endWith("府")){
                 fu.add(preList.get(i));
-            }else if(preList.get(i).contains("道")){
+            }else if(preList.get(i).endWith("道")){
                 dou.add(preList.get(i));
             }else{
                 to.add(preList.get(i));
@@ -72,7 +72,5 @@ public class Practice024 {
 */
 
     }//都道府県チェックメソッド呼び出し
-
-
 
 }//practice24終わり
--- src/main/java/practices/Practice025.java
+++ src/main/java/practices/Practice025.java
@@ -1,4 +1,9 @@
+
 package practices;
+import java.util.*;
+import java.util.stream.*;
+
+import static com.sun.jmx.snmp.ThreadContext.contains;
 
 /**
  * String型のArrayListを作成し、それに47都道府県を格納してください
@@ -11,15 +16,44 @@ package practices;
  *
  * 例）府を返却したいときに戻る値は 大阪府・京都府の2つが格納されたリストとなる
  *
- * ただし、for文は利用せず、Stream を利用してください。
+     * ただし、for文は利用せず、Stream を利用してください。
  *
  */
 
 public class Practice025 {
     public static void main(String[] args) {
-        // ここはご自由にお使いください
 
+        /*Stream型簡単なサンプルプログラム1
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Stream<Integer> stream = list.stream();
+        stream.forEach(System.out::println);
+        */
+        /*Stream型簡単なサンプルプログラム2
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5);
+        integerList.stream() // streamの取得
+                .filter(i -> i % 2 == 0) // 中間操作
+                .forEach(i -> System.out.println(i)); // 終端操作
+*/
+
+        // ここはご自由にお使いください
+        //都道府県を格納
+        ArrayList<String> prefecturesList = new ArrayList<>(Arrays.asList("北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"));
+        //都道府県チェックメソッド呼び出し
+        String order = "県";
+        //checkPre(prefecturesList, order);
+        System.out.println("戻り値確認："+checkPre(prefecturesList, order));
     }
 
+    public static List<String> checkPre(List<String> preList, String order) {
+
+        ArrayList<String> check = new ArrayList<>();
+        preList.stream()
+                .filter(i -> i.endsWith(order))
+                .forEach(i -> check.add(i));
+        System.out.println(check); //一応確認のため表示
+        return check;
+
+    }//checkメソッドend
 
 }
+
