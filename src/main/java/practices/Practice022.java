4febcd8651c6beb0e7a02aef172d7bacb28d367c
abi00867 <tatsuru.shimanuki@askul.com>
2019-04-15 11:29:12 +0900

    022の解答

--- src/main/java/practices/Practice022.java
+++ src/main/java/practices/Practice022.java
@@ -2,14 +2,45 @@ package practices;
 
 /**
  * 以前作成した素数判定関数を100000回実行したときの除算回数、処理時間をmsで出力しなさい
- *
  */
 
 public class Practice022 {
     public static void main(String[] args) {
+
+        //計測時間のsjの略
+
+        long start = System.nanoTime();
+        factorization(100000);
+        long end = System.nanoTime();
+        System.out.println("練習22の処理時間"+((end- start)/1000)+"ms");
+
         // ここはご自由にお使いください
 
     }
 
 
+    public static String factorization(int n) {
+
+        //計測時間を計測したい処理
+
+        String y = "";
+        for (int i = 2; n >= i; i++) {
+            while ((n % i) == 0) {
+                String x;
+                n = n / i;
+                x = Integer.toString(i);
+                if (n == 1) {
+                    y = y + x;
+                } else {
+                    y = y + x + " ";
+                }
+            }
+        }
+        return y;
+    }
+
+
 }
+
+
+
