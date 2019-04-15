125775d3fa348097a3d0a43b1e0ac312b3f2fba0
yama7 <h_yamana@askul.com>
2019-04-15 12:42:34 +0900

    Practice018の回答(山名）

--- src/main/java/practices/Practice018.java
+++ src/main/java/practices/Practice018.java
@@ -5,10 +5,9 @@ package practices;
  * 5の倍数と、5の付く数字のときに "buzz" と返却
  * どちらにもあてはまるときに "fizzbuzz" と返却
  * する関数 fizzbuzz を作成しなさい
- *
+ * <p>
  * 引数：整数
  * 返却："fizz" / "buzz" / "fizzbuzz" / "" (あてはまらない場合は空文字)
- *
  */
 
 public class Practice018 {
@@ -19,7 +18,19 @@ public class Practice018 {
 
     public static String fizzbuzz(int n) {
 
-        return "";
-    }
+        String m = String.valueOf(n);
 
+        if(n == 0){
+            return "";
+        }else if ((n % 3 == 0 || m.contains("3")) && (n % 5 == 0 || m.contains("5"))) {
+            return "fizzbuzz";
+        } else if (n % 3 == 0 || m.contains("3")) {
+            return "fizz";
+        } else if (n % 5 == 0 || m.contains("5")) {
+            return "buzz";
+        } else {
+            return "";
+        }
+
+    }
 }
