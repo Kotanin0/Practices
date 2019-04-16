ba531e499709ce5dfdd851cc86d7117ab588bc80
tao-ki <taoki@askul.com>
2019-04-16 10:58:35 +0900

    020 郵便番号・電話番号・メアド判定 青木

--- src/main/java/practices/Practice020.java
+++ src/main/java/practices/Practice020.java
@@ -6,49 +6,79 @@ package practices;
  * isPhoneNumber
  * isMailAddress
  * をそれぞれ作成しなさい
- *
+ * <p>
  * isPostNumber
  * 入力：文字列
  * 出力：true/false
  * 判定条件：7桁の連続した数値の文字列, 3桁-4桁の数値とハイフンで構成された文字列
  * 例：1234567=true, 123-4567=true, ...
- *
+ * <p>
  * isPhoneNumber
  * 入力：文字列
  * 出力：true/false
  * 判定条件：11桁の連続した数値の文字列, 数値とハイフン2つで構成された文字列
  * 例：09012345678=true, 090-1234-5678=true, 03-1234-5678=true, ...
- *
+ * <p>
  * isMailAddress
  * 入力：文字列
  * 出力：true/false
  * 判定条件：@を必ず含む, 末尾が ".com", ".co.jp", ".ne.jp" で終わる
  * 例：hoge@fuga.com=true, anonymous123@hoge.co.jp=true, ...
- *
  */
 
 public class Practice020 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-
+        boolean test;
+        test =isPhoneNumber("03-1123-4567");
+        System.out.println(test);
+        test =isPhoneNumber("044-123-4567");
+        System.out.println(test);
+        test =isPhoneNumber("0480-12-5678");
+        System.out.println(test);
+        test =isPhoneNumber("04805-1-5678");
+        System.out.println(test);
     }
 
     public static boolean isPostNumber(String s) {
         // 郵便番号：000-0000 または 0000000
         //
-
-
+        if (s.matches("^[0-9]{3}-[0-9]{4}$")) return true;
+        if (s.matches("^[0-9]{7}$")) return true;
         return false;
+
     }
 
     public static boolean isPhoneNumber(String s) {
-        // 電話番号：0X0-0000-0000 or 00-0000-0000 or 000-000-0000 or 0000-00-0000
+        // ・・・0で始まり、ハイフン2つで区切られ、末尾が必ず数字になる、12桁か13桁の数字
+        // （国内電話に限る）
+        // https://qiita.com/tomgoodsun/items/f3050689ac84827d5af9
+
+        // 電話番号（11桁）：0A0X0000000 or 0A0-X000-0000
+        // 0には0-9が入る　Aには2,5-9が入る　Xには1-9が入る
+        // ハイフンがある場合の挿入位置：前方は3字目の後で固定　後方は変動する
+        if (s.matches("^0[2|5-9]0[1-9][0-9]{7}$")) return true;
+        if (s.matches("^0[2|5-9]0-[1-9][0-9]*-[0-9]*$") && s.matches("^.{13}$")) return true;
+
+        // 電話番号（10桁）：0XX0000000
+        // or 0X-X000-0000 or 0XX-000-0000 or 0XX0-00-0000・・・
+        // 一般用の電話番号は、2桁目・3桁目には0以外が入る（たぶん）
+        // ハイフンがある場合の挿入位置は不定、必ず2つ
+        if (s.matches("^0[1-9]{2}[0-9]7$")) return true;
+        if (s.matches("^0[1-9]-[1-9][0-9]*-[0-9]*$") && s.matches("^.{12}$")) return true;
+        if (s.matches("^0[1-9]{2}([0-9]?|[0-9]*)-[0-9]*-[0-9]*$") && s.matches("^.{12}$")) return true;
+//        if (s.matches("^0[1-9]{2}-[0-9]*-[0-9]*$") && s.matches("^.{12}$")) return true;
+//        if (s.matches("^0[1-9]{2}[0-9]*-[0-9]*-[0-9]*$") && s.matches("^.{12}$")) return true;
 
         return false;
     }
 
     public static boolean isMailAddress(String s) {
-        //
+        // @を必ず含む, 末尾が ".com", ".co.jp", ".ne.jp" で終わる
+
+        if (s.matches("^[0-9a-zA-Z]+\\@[0-9a-zA-Z]+\\.com$")) return true;
+        if (s.matches("^[0-9a-zA-Z]+\\@[0-9a-zA-Z]+\\.co\\.jp$")) return true;
+        if (s.matches("^[0-9a-zA-Z]+\\@[0-9a-zA-Z]+\\.ne\\.jp$")) return true;
 
         return false;
     }
--- src/main/java/practices/Practice023.java
+++ src/main/java/practices/Practice023.java
@@ -1,64 +1,85 @@
 package practices;
 
-import jdk.internal.cmm.SystemResourcePressureImpl;
-
 import java.util.*;
 
 /**
  * おそらく素数判定関数の100000回の実行速度がとても遅いはずなので、速くしなさい
  */
 
+//      平方根使用前
+//        素数リスト作成時の除算：771632回
+//        100,000回試行したときの素数：12336個
+//        46ms
+//      平方根使用後
+//        素数リスト作成 15ms
+//        100,000回素数チェック 16ms
+//        素数リスト作成時の除算：38754回
+//        100,000回試行したときの素数：12400個
+
+
 public class Practice023 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-        long start = System.currentTimeMillis(); // 処理開始時間
         // 投入する素数の上限値
         int maxPrime = 10000;
 
         int countPrime[];
         countPrime = primeList(maxPrime);
 
-        long end = System.currentTimeMillis(); // 処理終了時間
         System.out.println("素数リスト作成時の除算：" + countPrime[0] + "回");
         System.out.println("100,000回試行したときの素数：" + countPrime[1] + "個");
-        System.out.println((end - start) + "ms");
     }
 
     // 素数リスト作る
     public static int[] primeList(int n) {
+        long start = System.currentTimeMillis(); // 処理開始時間
+
         // count[除算の回数, 素数の数] を格納していく
         int count[] = new int[2];
-        List<Integer> list = new ArrayList<Integer>(1500);
+        List<Integer> list = new ArrayList<Integer>(100);
 
         // 2を格納する
         list.add(2);
-        // 3以降はリストの数字で割れるかチェック
-        // 割れなければ素数→listに格納
+        // 3以降はリストの数字で割れなければ素数→listに格納
 
         // iは素数チェック対象（奇数だけ）
         // jは（既に格納した）リストから取り出す要素番号
+        double sqrt;
+        int listNum, listSize;
         loop:
-        for (int i = 3; i < n; i = i + 2) {
-            for (int j = 0; j < list.size(); j++) {
+        for (int i = 3; i < n; i += 2) {
+            sqrt = Math.sqrt(i);
+            listSize = list.size();
+            for (int j = 0; j < listSize; j++) {
+                // 平方根以上の値ではチェック不要
+                listNum = list.get(j);
+                if (listNum > sqrt) {
+                    continue;
+                }
                 count[0]++;
-                if (i % list.get(j) == 0) {
+                if (i % listNum == 0) {
                     continue loop;
                 }
             }
             list.add(i);
         }
         // 素数ちゃんと入ってるか確認用
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(list.get(i));
         }
-
-        Set<Integer> set = new HashSet<Integer>(list);
+        System.out.println("素数リストの数" + list.size());
+        long lap = System.currentTimeMillis(); // リスト作成終了時間
+        System.out.println("素数リスト作成 " + (lap - start) + "ms");
 
         // 100000回の素数チェック
+        Set<Integer> set = new HashSet<Integer>(list);
         Random random = new Random();
         for (int i = 0; i < 100000; i++) {
-            count[1]+=isPrime(set, random.nextInt(n));
+            count[1] += isPrime(set, random.nextInt(n));
         }
+
+        long end = System.currentTimeMillis(); // 処理終了時間
+        System.out.println("100,000回素数チェック " + (end - lap) + "ms");
 
         return count;
     }
@@ -85,7 +106,7 @@ public class Practice023 {
         // iは素数チェック対象（奇数だけ）
         loop:
         for (int i = 3; i < n; i = i + 2) {
-            for (Integer j:set) {
+            for (Integer j : set) {
                 count[0]++;
                 if (i % j == 0) {
                     continue loop;
@@ -94,7 +115,7 @@ public class Practice023 {
             set.add(i);
         }
         // 素数ちゃんと入ってるか確認用
-        for (int j:set) {
+        for (int j : set) {
             System.out.println(j);
         }
         System.out.println(count[0]);
