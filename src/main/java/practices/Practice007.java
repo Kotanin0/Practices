7ba77323c60bb711331ebeb521dd16df522cbbde
yama7 <h_yamana@askul.com>
2019-04-16 14:53:24 +0900

    Practice021の回答(山名）

--- src/main/java/practices/Practice007.java
+++ src/main/java/practices/Practice007.java
@@ -11,7 +11,7 @@ public class Practice007 {
 
     public static void main(String[] args) {
         // ここはご自由にお使いください
-        System.out.println(isPrime(5));
+        System.out.println(isPrime(11));
 
     }
 
@@ -19,18 +19,17 @@ public class Practice007 {
     // 素数判定
     public static boolean isPrime(int n) {
 
-        if (n <= 0) {
-            throw new IllegalArgumentException("自然数で入力してください");
-        }
-        if (n == 1) {
+        //if (n <= 0) {
+        //    throw new IllegalArgumentException("自然数で入力してください");
+        //}
+        if (n == 1 || n % 2 == 0 || n<= 0) {
             return false;
         }
         for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                return false;
+            if (n % i != 0) {
+                return true;
             }
-
         }
-        return true;
+        return false;
     }
 }
--- src/main/java/practices/Practice022.java
+++ src/main/java/practices/Practice022.java
@@ -1,15 +1,34 @@
 package practices;
 
+import static practices.Practice007.isPrime;
+
 /**
  * 以前作成した素数判定関数を100000回実行したときの除算回数、処理時間をmsで出力しなさい
- *
  */
 
 public class Practice022 {
     public static void main(String[] args) {
-        // ここはご自由にお使いください
 
+        // ここはご自由にお使いください
+        long startTime = System.currentTimeMillis();
+
+        int i =0;
+
+        for (int k = 0; k < 100000; k++) {
+
+            isPrime(k);
+
+            i += 1;
+        }
+
+        // 処理後の時刻を取得
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("徐算回数：" + i + "回");
+        System.out.println("処理時間：" + (endTime - startTime) + " ms");
     }
 
-
 }
+
+
+
