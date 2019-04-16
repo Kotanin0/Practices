fefc665f9ae29b0f747f8df5e6c8d441cf220835
yutaka0m <yutaka.miyamae@askul.com>
2019-04-16 09:57:18 +0900

    029の解答

--- src/main/java/practices/Fruits.java
+++ src/main/java/practices/Fruits.java
@@ -51,4 +51,15 @@ public class Fruits {
         System.out.println("果実名：" + this.fruitsNameJapanese + "(" + this.fruitsNameEnglish + ")");
         System.out.println("在庫数：" + this.stocks);
     }
+
+    public void printNamePriceAndStock() {
+        System.out.println("果実名：" + this.fruitsNameJapanese + "(" + this.fruitsNameEnglish + ")");
+        System.out.println(("単価：" + this.price));
+        System.out.println("在庫数：" + this.stocks);
+    }
+
+    public void buyUpAlltheFruit() {
+        System.out.println(this.fruitsNameJapanese + "（" + this.price + "円） " + this.stocks + "個 " + this.price * this.stocks + "円 お買い上げありがとうございます");
+        this.stocks = 0;
+    }
 }
--- src/main/java/practices/Practice029.java
+++ src/main/java/practices/Practice029.java
@@ -1,23 +1,37 @@
 package practices;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * さらに果物リストを続けて利用し、以下の関数を作成しなさい
- *
+ * <p>
  * 一番単価が高い果物を在庫全て買い占める関数
  * 名前：お好きに
  * 引数：お好きに
  * 戻り値：合計金額
- *
+ * <p>
  * その関数を利用したときに、コンソールには以下のように出力されるイメージ
  * > 世界一りんご（2000円） 100個 200000円 お買い上げありがとうございます
- *
  */
 
 public class Practice029 {
     public static void main(String[] args) {
-        // ここはご自由にお使いください
+        List<Fruits> fruits = Practice027.returnFruitsList();
 
+        fruits = buyAllStockFromMostExpenciveFruit(fruits);
+        for (Fruits f : fruits) {
+            f.printNamePriceAndStock();
+        }
     }
 
+    public static List<Fruits> buyAllStockFromMostExpenciveFruit(List<Fruits> fruits) {
+        List<Fruits> x = fruits.stream()
+                .sorted((fruit1, fruit2) -> fruit2.getPrice() - fruit1.getPrice())
+                .collect(Collectors.toList());
 
+        x.get(0).buyUpAlltheFruit();
+        return x;
+    }
 }
