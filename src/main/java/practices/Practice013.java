02f70e56561cc54c697567e673ab3d5723627216
takeuchi <ryuta.takeuchi@askul.com>
2019-04-16 16:08:52 +0900

    013回答です

--- src/main/java/practices/Practice013.java
+++ src/main/java/practices/Practice013.java
@@ -1,14 +1,15 @@
 package practices;
 
+import java.util.Arrays;
+
 /**
  * 配列の中身を小さい順に並べ替える関数 sort を作成しなさい
- *
+ * <p>
  * 引数：整数の配列
  * 返却：整数の配列
- *
+ * <p>
  * 　例）引数：[0, 2, 3, 1, 9, 7, 6, 4, 5, 8]
  * 　　　返却：[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
- *
  */
 
 public class Practice013 {
@@ -20,6 +21,7 @@ public class Practice013 {
     // 並べ替えられた配列を返す
     public static int[] sort(int[] n) {
 
+        Arrays.sort(n);
         return n;
     }
 
