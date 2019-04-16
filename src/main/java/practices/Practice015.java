6d8f0578bfffb38a8e40bc0f7d3d83368bdd1bfd
abi01120 <fumiaki.tsuchiya@askul.com>
2019-04-16 15:41:49 +0900

    土屋_Practice015の解答

--- src/main/java/practices/Practice015.java
+++ src/main/java/practices/Practice015.java
@@ -2,22 +2,21 @@ package practices;
 
 /**
  * 入力された文字の中に "ge" という文字列が含まれているか判定する関数 ge を作成しなさい
- *
+ * <p>
  * 引数：文字列
  * 返却：true/false
- *
  */
 
 public class Practice015 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-
     }
 
     // "ge" という文字列が含まれているならtrue
     public static boolean ge(String str) {
-
-        return true;
+        if (str.contains("ge")) {
+            return true;
+        }
+        return false;
     }
-
 }
