d9ceb0a0615964c26f2c9580b2867d01a99e1946
tao-ki <taoki@askul.com>
2019-04-17 16:19:09 +0900

    102 ItemGroupクラス 青木

--- src/main/java/practices/items/Item.java
+++ src/main/java/practices/items/Item.java
@@ -17,20 +17,20 @@ public class Item {
     public String itemName, itemCopy, itemMakerName;
     public int itemPrice, quantity;
 
-    Item(String itemName,String itemCopy,String itemMakerName,int itemPrice, int quantity){
-        this.itemName=itemName;
-        this.itemCopy=itemCopy;
-        this.itemMakerName=itemMakerName;
-        this.itemPrice=itemPrice;
-        this.quantity=quantity;
+    Item(String itemName, String itemCopy, String itemMakerName, int itemPrice, int quantity) {
+        this.itemName = itemName;
+        this.itemCopy = itemCopy;
+        this.itemMakerName = itemMakerName;
+        this.itemPrice = itemPrice;
+        this.quantity = quantity;
     }
 
-    public void printData(){
-        System.out.println("＝＝＝＝＝＝＝＝＝＝");
-        System.out.println("商品名："+this.itemName);
-        System.out.println("商品キャッチコピー："+this.itemCopy);
-        System.out.println("メーカー名："+this.itemMakerName);
-        System.out.println("価格："+this.itemPrice);
-        System.out.println("在庫数："+this.quantity);
+    public void printData() {
+        System.out.println("－－－－－－－－－");
+        System.out.println("商品名：" + this.itemName);
+        System.out.println("商品キャッチコピー：" + this.itemCopy);
+        System.out.println("メーカー名：" + this.itemMakerName);
+        System.out.println("価格：" + this.itemPrice);
+        System.out.println("在庫数：" + this.quantity);
     }
 }
