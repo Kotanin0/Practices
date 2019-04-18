94046af4f0460b16cdc12cd54704a3fa9ec7d015
takeuchi <ryuta.takeuchi@askul.com>
2019-04-18 15:02:48 +0900

    020回答です

--- src/main/java/practices/Practice020.java
+++ src/main/java/practices/Practice020.java
@@ -1,5 +1,9 @@
 package practices;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 /**
  * 入力された文字列が郵便番号、電話番号、メールアドレス、それ以外のいずれかを判定する関数
  * isPostNumber
@@ -27,25 +31,45 @@ package practices;
  *
  */
 
+
 public class Practice020 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
 
+        System.out.println(isMailAddress("test@test.co.jp"));
     }
 
     public static boolean isPostNumber(String s) {
+        //7桁 or 3桁+"-"+4桁
+
+        if (s.matches("^[0-9]{7}$")) return true;
+        if (s.matches("^[0-9]{3}-[0-9]{4}$")) return true;
 
         return false;
     }
 
     public static boolean isPhoneNumber(String s) {
+        //11桁 or 含む<3桁+"-"+4桁+"-"> or 含む<2桁+"-"+4桁+"-">
+        //"^[0-9]{3}[0-9]{4}[0-9]{4}$"
+        //"^[0-9]{3}-[0-9]{4}-[0-9]{4}$"
+        //"^0\d-\d{4}-\d{4}$"
+
+        if (s.matches("^[0-9]{3}[0-9]{4}[0-9]{4}$")) return true;
+        if (s.matches("^[0-9]{3}-[0-9]{4}-[0-9]{4}$")) return true;
+        //if(s.matches("^0\d-\d{4}-\d{4}$")) return true;
 
         return false;
     }
 
     public static boolean isMailAddress(String s) {
+        //ローカルパート正しい　かつ　ドメイン名はこの三種類
+
+        if (s.matches("..*@.*") && (s.endsWith(".com") || s.endsWith(".co.jp") || s.endsWith(".ne.jp"))) return true;
 
         return false;
     }
 
 }
+
+
+
