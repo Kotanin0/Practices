496a50ceb5331903d9c33d58a3f1e60d583ad254
keitaoku <keita.okuhira@askul.com>
2019-04-18 16:44:25 +0900

    Practice026の解答

--- src/main/java/practices/Practice026.java
+++ src/main/java/practices/Practice026.java
@@ -1,19 +1,34 @@
 package practices;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * String型のArrayListを作成し、それに47都道府県を格納してください
- *
+ * <p>
  * 格納したListから、任意の文字列で都道府県を検索できる関数を作りなさい
- *
+ * <p>
  * 関数名、引数、戻り値の指定はしません
- *
  */
 
 public class Practice026 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-
+        ArrayList<String> prefectureName47 = new ArrayList<String>(Arrays.asList("北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県",
+                "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県",
+                "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
+                "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県",
+                "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"));
+        //nは漢字のみに限る
+        System.out.println(getResearchPrefectureName(prefectureName47, "石"));
     }
 
-
+    public static List<String> getResearchPrefectureName(List<String> prefectureName47, String n) {
+        return prefectureName47.stream()
+                .filter(s -> s.contains(n))
+                .collect(Collectors.toList());
+    }
 }
+
