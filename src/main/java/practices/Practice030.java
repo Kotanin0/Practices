d53f0788da9828b132c1658ffea340383c8f2067
yutaka0m <yutaka.miyamae@askul.com>
2019-04-19 17:39:27 +0900

    030の準備（変更が簡単になるように）

--- src/main/java/practices/Practice030.java
+++ src/main/java/practices/Practice030.java
@@ -1,51 +1,78 @@
 package practices;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Practice019 で作成した coincheck を拡張しなさい
- *
+ * <p>
  * 仕様追加）
  * ・元号が変わったので、新しい硬貨ができました！
  * 　250円玉、25円玉
  * ・硬貨の枚数が0枚の時は、出力しなくて良い
  * ・500円玉が切れている場合があるらしいので、500円玉でかえす or かえさないを引数に追加してください
- *
+ * <p>
  * 提出方法）
  * ・Practice019のcoincheckを、本クラスにコピーして、まずプルリクエストを出してください
  * ・その後、解いてください
- *
  */
 
 public class Practice030 {
     public static void main(String[] args) {
-        // ここはご自由にお使いください
-        System.out.println(coincheck(99999));
+        System.out.println(("--250円--"));
+        System.out.println(coincheck(250, Coin.fiveHundred));
+        System.out.println(("--25円--"));
+        System.out.println(coincheck(25, Coin.fiveHundred));
+        System.out.println(("--27円--"));
+        System.out.println(coincheck(27, Coin.one));
     }
 
-    public static String coincheck(int n){
-        if (n <= 0) return "";
+    public static String coincheck(int n, Coin emptyCoin) {
+        if (n < 1) return "";
 
-        int[] monetaryUnit = {500,100,50,10,5,1};//貨幣の種類
-        int[] returnCoin =   {  0,  0, 0, 0,0,0};//貨幣単位ごとの枚数を格納する
-
-        //500円はnを500で割ったときの商
-        returnCoin[0] = n / monetaryUnit[0];
-
-        //500円未満は、1つ前の貨幣の剰余の商
-        for (int i = 1; i < monetaryUnit.length; i++) {
-            returnCoin[i] = (n % monetaryUnit[i-1]) / monetaryUnit[i];
-        }
-
-        //返り値作成
-        String returnStr = "";
         String br = System.getProperty("line.separator");
-        int j;
-        for (j = 0; j < monetaryUnit.length-1; j++) {
-            if (returnCoin[j] != 0) {
-                returnStr += monetaryUnit[j] + "円" + "＝" + returnCoin[j] + "枚" + br;
-            }
+        String resultStr = "";
+        int someCoins;
+
+        for (Coin coin : Coin.values()) {
+            if (coin == emptyCoin) continue;
+            someCoins = n / coin.getCoinNameInt();//枚数
+            if (someCoins == 0) continue;
+            n = n % coin.getCoinNameInt();//あまり
+            resultStr += coin.getCoinNameStr() + "＝" + someCoins + "枚" + br;
         }
-        //最後は改行なし
-        returnStr += monetaryUnit[j] + "円" + "＝" + returnCoin[j] + "枚";
-        return returnStr;
+
+        if (n != 0) {
+            resultStr += "残り" + n + "円返せませんでした";
+        }
+
+        return resultStr.trim();
+    }
+
+    protected enum Coin {
+        fiveHundred(500, "500円"),
+        oneHundred(100, "100円"),
+        fifty(50, "50円"),
+        ten(10, "10円"),
+        five(5, "5円"),
+        one(1, "1円");
+
+        private int coinNameInt;
+        private String coinNameStr;
+
+        Coin(int coinNameInt, String coinNameStr) {
+            this.coinNameInt = coinNameInt;
+            this.coinNameStr = coinNameStr;
+        }
+
+        public int getCoinNameInt() {
+            return this.coinNameInt;
+        }
+
+        public String getCoinNameStr() {
+            return this.coinNameStr;
+        }
     }
 }
