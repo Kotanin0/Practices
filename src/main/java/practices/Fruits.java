dc52207f869bb587e5ed14d00bba1a98c2ee959a
abi00867 <tatsuru.shimanuki@askul.com>
2019-04-19 18:03:05 +0900

    020の解答

--- src/main/java/practices/Fruits.java
+++ src/main/java/practices/Fruits.java
@@ -7,9 +7,46 @@ package practices;
  * - 色
  * - 単価
  * - 在庫数
- *
  */
 
 public class Fruits {
 
+    private String englishName;
+    private String japanName;
+    private String color;
+    private int price;
+    private int stock;
+
+
+    Fruits(String englishName, String japanName, String color, int tanka, int stock) {
+
+        this.englishName = englishName;
+        this.japanName = japanName;
+        this.color = color;
+        this.price = tanka;
+        this.stock = stock;
+
+
+    }
+
+    public String getEnglishName() {
+        return this.englishName;
+    }
+
+    public String getJapanName() {
+        return this.japanName;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
 }
+
