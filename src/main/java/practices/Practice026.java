4c4618b01e58cd82ab109a64f2d6f60f2222abd1
yama7 <h_yamana@askul.com>
2019-04-19 18:18:48 +0900

    Practice024-26の回答(山名） リストへの47都道府県の追加方法を見直し

--- src/main/java/practices/Practice026.java
+++ src/main/java/practices/Practice026.java
@@ -1,6 +1,7 @@
 package practices;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,57 +16,12 @@ import java.util.List;
 public class Practice026 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-        // ここはご自由にお使いください
 
-        ArrayList<String> jpList = new ArrayList<String>();
 
-        jpList.add("北海道");
-        jpList.add("青森県");
-        jpList.add("岩手県");
-        jpList.add("宮城県");
-        jpList.add("秋田県");
-        jpList.add("山形県");
-        jpList.add("福島県");
-        jpList.add("茨城県");
-        jpList.add("栃木県");
-        jpList.add("群馬県");
-        jpList.add("埼玉県");
-        jpList.add("千葉県");
-        jpList.add("東京都");
-        jpList.add("神奈川県");
-        jpList.add("新潟県");
-        jpList.add("富山県");
-        jpList.add("石川県");
-        jpList.add("福井県");
-        jpList.add("山梨県");
-        jpList.add("長野県");
-        jpList.add("岐阜県");
-        jpList.add("静岡県");
-        jpList.add("愛知県");
-        jpList.add("三重県");
-        jpList.add("滋賀県");
-        jpList.add("京都府");
-        jpList.add("大阪府");
-        jpList.add("兵庫県");
-        jpList.add("奈良県");
-        jpList.add("和歌山県");
-        jpList.add("鳥取県");
-        jpList.add("島根県");
-        jpList.add("岡山県");
-        jpList.add("広島県");
-        jpList.add("山口県");
-        jpList.add("徳島県");
-        jpList.add("香川県");
-        jpList.add("愛媛県");
-        jpList.add("高知県");
-        jpList.add("福岡県");
-        jpList.add("佐賀県");
-        jpList.add("長崎県");
-        jpList.add("熊本県");
-        jpList.add("大分県");
-        jpList.add("宮崎県");
-        jpList.add("鹿児島県");
-        jpList.add("沖縄県");
+        ArrayList<String> jpList = new ArrayList<String>(Arrays.asList("北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県",
+                "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県",
+                "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県",
+                "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"));
 
         System.out.println(getPrifecture("山",jpList));
 
