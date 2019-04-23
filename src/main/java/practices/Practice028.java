9557c162f8e7b9eeac1c3d5f352e404025c7db06
tao-ki <taoki@askul.com>
2019-04-23 19:34:49 +0900

    027-029 フルーツ問題 青木

--- src/main/java/practices/Practice028.java
+++ src/main/java/practices/Practice028.java
@@ -1,23 +1,93 @@
 package practices;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 /**
  * Practice027で作成した果物リストを用いて、以下の関数をそれぞれ作成しなさい
- *
+ * <p>
  * - 一番単価の高い果物を返す関数
- *
+ * <p>
  * - 任意の色の果物を返す関数
- *
+ * <p>
  * - 在庫数が多い順で果物を返す関数
- *
+ * <p>
  * - 在庫がある果物のみを返す関数
- *
  */
 
 public class Practice028 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
 
+        List<Fruits> fruit = Practice027.makeFruitsList();
+
+        fruit.stream()
+                .map(p -> p.getNameJpn())
+                .forEach(System.out::println);
+
+        String searchColor = "pink";
+
+        System.out.println("▼一番高い果物は：");
+        mostExpensiveFruit(fruit).stream()
+                .forEach(a -> System.out.println(a.getNameJpn() + " " + a.getPrice() + "円"));
+
+        System.out.println("▼" + searchColor + "色の果物は：");
+        searchColorFruit(fruit, searchColor).stream()
+                .forEach(a -> System.out.println(a.getNameJpn() + " " + a.getColor() + "色"));
+
+        System.out.println("▼在庫の多い順：");
+        sortInDescendingStock(fruit).stream()
+                .forEach(a -> System.out.println(a.getNameJpn() + " " + a.getStock() + "個"));
+
+        System.out.println("▼在庫がある果物は：");
+        hasStockFruit(fruit).stream()
+                .forEach(a -> System.out.println(a.getNameJpn() + " " + a.getStock() + "個"));
+
     }
 
+    public static List<Fruits> mostExpensiveFruit(List<Fruits> fruit) {
+        Integer maxPrice = fruit.stream()
+                .map(Fruits::getPrice)
+                .max(Comparator.naturalOrder())
+                .get();
 
+        List<Fruits> resultFruit = fruit.stream()
+                .filter(s -> s.getPrice() == maxPrice)
+                .collect(Collectors.toList());
+
+//        List<Fruits> resultFruit = fruit.stream()
+//                .sorted((fruit1, fruit2) -> fruit2.getPrice() - fruit1.getPrice())
+//                .collect(Collectors.toList());
+
+//        List<Fruits> resultFruit = fruit.stream()
+//                     .max((p1, p2) -> Fruits.compare(p1.getPrice(), p2.getPrice()))
+//                .get();
+        return resultFruit;
+    }
+
+    public static List<Fruits> searchColorFruit(List<Fruits> fruit, String searchColor) {
+        List<Fruits> resultFruit = fruit.stream()
+                .filter(s -> s.getColor() == searchColor)
+                .collect(Collectors.toList());
+
+        return resultFruit;
+    }
+
+    public static List<Fruits> sortInDescendingStock(List<Fruits> fruit) {
+        List<Fruits> resultFruit = fruit.stream()
+                .sorted(Comparator.comparingInt(Fruits::getStock).reversed())
+                .collect(Collectors.toList());
+
+        return resultFruit;
+    }
+
+    public static List<Fruits> hasStockFruit(List<Fruits> fruit) {
+        List<Fruits> resultFruit = fruit.stream()
+                .filter(s -> s.getStock() > 0)
+                .collect(Collectors.toList());
+
+        return resultFruit;
+    }
 }
--- src/main/java/practices/Practice029.java
+++ src/main/java/practices/Practice029.java
@@ -1,21 +1,27 @@
 package practices;
 
+import java.util.List;
+
 /**
  * さらに果物リストを続けて利用し、以下の関数を作成しなさい
- *
+ * <p>
  * 一番単価が高い果物を在庫全て買い占める関数
  * 名前：お好きに
  * 引数：お好きに
  * 戻り値：合計金額
- *
+ * <p>
  * その関数を利用したときに、コンソールには以下のように出力されるイメージ
  * > 世界一りんご（2000円） 100個 200000円 お買い上げありがとうございます
- *
  */
 
 public class Practice029 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
+        List<Fruits> fruit = Practice027.makeFruitsList();
+
+        Practice028.mostExpensiveFruit(fruit).stream()
+                .forEach(a -> System.out.println(a.getNameJpn() + "（" + a.getPrice() + "円）" + a.getStock() + "個" + a.getPrice() * a.getStock() + "円"));
+        System.out.println("お買い上げありがとうございます");
 
     }
 
