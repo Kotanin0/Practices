974b31fd7c201f9a8e2aa824a888ed1008915bd2
keitaoku <keita.okuhira@askul.com>
2019-04-24 10:18:59 +0900

    practice030の解答

--- src/main/java/practices/Practice030.java
+++ src/main/java/practices/Practice030.java
@@ -17,54 +17,36 @@ package practices;
 public class Practice030 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-
+        System.out.println(coincheck(1238, "false"));
     }
 
-    public static String coincheck(int n) {
+    public static String coincheck(int n, String check) {
         String br = System.getProperty("line.separator");
         //0以下の数値を空で返す
         if (0 >= n) return "";
-        //各紙幣の割れる枚数を特定する。
-        //割る作業をループさせる→割った数値をカウントする
-        //割った後にNを更新させる
-        int a500 = 0;
-        int a100 = 0;
-        int a50 = 0;
-        int a10 = 0;
-        int a5 = 0;
-        int a1 = 0;
 
-        while (n != 0) {
-            while (n >= 500) {
-                a500++;
-                n = n - 500;
+        String getCoinCheck = "";
+        int count = 0;
+        int[] coins = {500, 250, 100, 50, 25, 10, 5, 1};
+
+        if (check == "true") {
+            for (int i = 0; i < coins.length; i++) {
+                if (n > coins[i]) {
+                    count = n / coins[i];
+                    n %= coins[i];
+                    getCoinCheck += coins[i] + "円=" + count + "枚" + br;
+                }
             }
-            while (n >= 100) {
-                a100++;
-                n = n - 100;
-            }
-            while (n >= 50) {
-                a50++;
-                n = n - 50;
-            }
-            while (n >= 10) {
-                a10++;
-                n = n - 10;
-            }
-            while (n >= 5) {
-                a5++;
-                n = n - 5;
-            }
-            while (n >= 1) {
-                a1++;
-                n = n - 1;
+            return getCoinCheck;
+        } else {
+            for (int i = 1; i < coins.length; i++) {
+                if (n > coins[i]) {
+                    count = n / coins[i];
+                    n %= coins[i];
+                    getCoinCheck += coins[i] + "円=" + count + "枚" + br;
+                }
             }
         }
-        return  "500円" + "＝" + a500 + "枚" + br +
-                "100円" + "＝" + a100 + "枚" + br +
-                "50円" + "＝" + a50 + "枚" + br +
-                "10円" + "＝" + a10 + "枚" + br +
-                "5円" + "＝" + a5 + "枚" + br +
-                "1円" + "＝" + a1 + "枚";
+        return getCoinCheck;
     }
 }
