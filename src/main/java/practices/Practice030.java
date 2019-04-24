448dbaba781315d467c54197dbb1e92fbeb75d1c
Sakamune0 <junji.sakamune@askul.com>
2019-04-24 14:11:40 +0900

    Practice30のレビューお願いいたします。

--- src/main/java/practices/Practice030.java
+++ src/main/java/practices/Practice030.java
@@ -17,29 +17,25 @@ package practices;
 public class Practice030 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
+        int coin = 858;
+        int[] coinName = {500, 250, 100, 50, 25, 10, 5, 1};
+        int[] hasCoin = {0, 1, 1, 1, 1, 1, 1, 1};  //500円がない場合を引数に追加。
+        System.out.println("商品金額");
+        System.out.println(coincheck(coin, coinName, hasCoin));
+
     }
 
-    public static String coincheck(int n) {
-        if (n <= 0) {
-            return "";
-        }
+    public static String coincheck(int coin, int[] coinName, int[] hasCoin) {
+        if (coin <= 0) return "";
+        String br = System.getProperty("line.separator"); //段落
+        String strReturn = ""; //空箱
+        int[] count = new int[coinName.length];
 
-        int count = 0;
-        int[] coinName = {500, 100, 50, 10, 5, 1};
-
-        //改行コード
-        String br = System.getProperty("line.separator");
-        //空箱設定
-        String strReturn = "";
-
-        for (
-                int i = 0;
-                i < coinName.length; i++) {
-            if (n > coinName[i]) {
-                count = n / coinName[i];
-                n = n % coinName[i];
-                strReturn += coinName[i] + "円＝" + count + "枚" + br;
-            }
+        for (int i = 0; i < coinName.length; i++) {
+            //コインが0枚の場合または、coinCountが0以下のときは余りの計算をスキップする。
+            if (hasCoin[i] == 0 || (count[i] = coin / coinName[i]) <= 0) continue;
+            coin %= coinName[i];
+            strReturn += coinName[i] + "円＝" + count[i] + "枚" + br;
         }
         return strReturn.trim();
     }
