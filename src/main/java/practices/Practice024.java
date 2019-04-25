f3b53eda2f5dd0c2bcdc15aaf8d03deddae4f458
abi00546 <stoyama@askul.com>
2019-04-25 09:11:19 +0900

    practice024の解答（再修正版）

--- src/main/java/practices/Practice024.java
+++ src/main/java/practices/Practice024.java
@@ -1,6 +1,6 @@
 package practices;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,53 +19,44 @@ public class Practice024 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
 
-        prefectureType("都"); //
+        isPrefectureType("県"); //
     }
 
-    public static String prefectureType(String str) {
+    public static String isPrefectureType(String str) {
 
         String prefecture[] = {"北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"};
+        String prefectureType[] = {"都", "道", "府", "県"};
 
-        String to = "都";
-        List<String> toList = new ArrayList<String>();
-        String dou = "道";
-        List<String> douList = new ArrayList<String>();
-        String fu = "府";
-        List<String> fuList = new ArrayList<String>();
-        String ken = "県";
-        List<String> kenList = new ArrayList<String>();
+        String pref = str;
+        List<String> list = Arrays.asList(prefectureType); //配列をList型オブジェクトに変換
+        int n = list.indexOf(pref); //list内に引数の要素が存在する最小のインデックスを返す
 
         for (int i = 0; i < prefecture.length; i++)
-            if (prefecture[i].endsWith(to)) {
-                toList.add(prefecture[i]);
-            } else if (prefecture[i].endsWith(dou)) {
-                douList.add(prefecture[i]);
-            } else if (prefecture[i].endsWith(fu)) {
-                fuList.add(prefecture[i]);
-            } else if (prefecture[i].endsWith(ken)) {
-                kenList.add(prefecture[i]);
+            if (prefecture[i].endsWith(prefectureType[n])) {
+                System.out.println(prefecture[i] + ",");
             }
-
-        if (str == to) {
-            for (int j = 0; j < toList.size(); j++) {
-                System.out.println(toList.get(j));
-            }
-        } else if (str == dou) {
-            for (int j = 0; j < douList.size(); j++) {
-                System.out.println(douList.get(j));
-            }
-        } else if (str == fu) {
-            for (int j = 0; j < fuList.size(); j++) {
-                System.out.println(fuList.get(j));
-            }
-        } else if (str == ken) {
-            for (int j = 0; j < kenList.size(); j++) {
-                System.out.println(kenList.get(j));
-            }
-        }
-        return "都道府県ないやつを入力してるよ";
+        return "";
     }
 }
+
+//　色々やってみたけど、最後のカンマをなくせない…
+//        if (str == prefectureType[0]) {
+//            for (int j = 0; j < toList.size(); j++) {
+//                System.out.println(toList.get(j));
+//            }
+//        } else if (str == prefectureType[1]) {
+//            for (int j = 0; j < douList.size(); j++) {
+//                System.out.println(douList.get(j));
+//            }
+//        } else if (str == prefectureType[2]) {
+//            for (int j = 0; j < fuList.size(); j++) {
+//                System.out.println(fuList.get(j));
+//            }
+//        } else if (str == prefectureType[3]) {
+//            for (int j = 0; j < kenList.size(); j++) {
+//                System.out.println(kenList.get(j));
+//            }
+//        }
 
 
 // "府"って入れたらfuListをリターンしたい
@@ -84,3 +75,18 @@ public class Practice024 {
 //        System.out.print(prefectureList.get(j));
 //        }
 //        }
+
+//        String to = "都";
+//        String dou = "道";
+//        String fu = "府";
+//        String ken = "県";
+
+//                toList.add(prefecture[i]);
+//                douList.add(prefecture[i]);
+//                fuList.add(prefecture[i]);
+//                kenList.add(prefecture[i]);
+
+//    List<String> toList = new ArrayList<String>();
+//    List<String> douList = new ArrayList<String>();
+//    List<String> fuList = new ArrayList<String>();
+//    List<String> kenList = new ArrayList<String>();
--- src/main/java/practices/Practice026.java
+++ src/main/java/practices/Practice026.java
@@ -1,5 +1,9 @@
 package practices;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 /**
  * String型のArrayListを作成し、それに47都道府県を格納してください
  *
@@ -12,8 +16,22 @@ package practices;
 public class Practice026 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
+        prefectureType("大");
 
     }
 
+    public static String prefectureType(String str) {
+
+//        String prefecture[] = {"北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"};
+//        ArrayList<String> prefectureList = new ArrayList<String>();
+
+        List<String> prefecturerList = Arrays.asList("北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県");
+        prefecturerList.stream() // streamの取得
+                .filter(i -> i.contains(str)) // 中間操作
+                .forEach(i -> System.out.println(i)); // 終端操作
+
+        return "";
+    }
 
 }
+
