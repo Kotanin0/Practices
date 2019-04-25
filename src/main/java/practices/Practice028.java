7e5ff4202fbeca0ed9f995bd9a04a4d9d54ebad7
keitaoku <keita.okuhira@askul.com>
2019-04-25 09:53:49 +0900

    practice028、29の解答（最新）

--- src/main/java/practices/Practice028.java
+++ src/main/java/practices/Practice028.java
@@ -6,11 +6,12 @@ import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
 
+
 /**
  * Practice027で作成した果物リストを用いて、以下の関数をそれぞれ作成しなさい
  * <p>
  * - 一番単価の高い果物を返す関数
- * <p>
+ * * <p>
  * - 任意の色の果物を返す関数
  * <p>
  * - 在庫数が多い順で果物を返す関数
@@ -21,47 +22,72 @@ import static java.util.Comparator.comparing;
 public class Practice028 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-        System.out.println(maxPriceFruits());
-        System.out.println(fruitsColor("red"));
-        System.out.println(manyStockFruits());
-        System.out.println(inStockFruits());
+        List<Fruits> getFruitsList = Practice027.fruitsList();
 
+        //一番単価の高い果物
+        System.out.println("=====一番単価が高い果物=====");
+        //Fruits maxPriceFruitsList = maxPriceFruits(getFruitsList);
+        //maxPriceFruitsList.printData();
+        System.out.println(maxPriceFruits(getFruitsList));
+
+
+        //任意の色の果物
+        System.out.println("=====赤色の果物=====");
+        List<Fruits> fruitsColorList = fruitsColor(getFruitsList, "red");
+        for (Fruits i : fruitsColorList) {
+            i.printData();
+        }
+
+        //在庫数が多い順で果物
+        System.out.println("=====在庫が多い順=====");
+        List<Fruits> manyStockFruitsList = manyStockFruits(getFruitsList);
+        for (Fruits i : manyStockFruitsList) {
+            i.printData();
+        }
+
+        //在庫がある果物
+        System.out.println("=====在庫がある商品=====");
+        List<Fruits> inStockFruitsList = inStockFruits(getFruitsList);
+        for (Fruits i : inStockFruitsList) {
+            i.printData();
+        }
     }
 
     //一番単価の高い果物を返す関数
-    public static String maxPriceFruits() {
-            List<Fruits> newFruitsList = Practice027.fruitsList();
-            return newFruitsList.stream()
-                    .max(Comparator.comparing(s -> s.getPrice()))
-                    .map(s -> s.getName())
-                    .get();
+    public static String maxPriceFruits(List<Fruits> getFruitsList) {
+        return getFruitsList.stream()
+                .max(Comparator.comparing(s -> s.getPrice()))
+                .map(s -> s.getName())
+                .get();
     }
 
     //任意の色の果物を返す関数
-    public static List<String> fruitsColor(String color) {
-        List<Fruits> newFruitsList = Practice027.fruitsList();
-        return newFruitsList.stream()
-                .filter(s -> s.getColor().equals(color))
-                .map(s -> s.getName())
+    public static List<Fruits> fruitsColor(List<Fruits> getFruitsList, String color) {
+        return getFruitsList.stream()
+                .filter(s -> s.getColor().matches(color))
                 .collect(Collectors.toList());
     }
 
     //在庫数が多い順で果物を返す関数
-    public static List<String> manyStockFruits() {
-        List<Fruits> newFruitsList = Practice027.fruitsList();
-        return newFruitsList.stream()
+    public static List<Fruits> manyStockFruits(List<Fruits> getFruitsList) {
+        return getFruitsList.stream()
                 .sorted(comparing(Fruits::getStock).reversed())
-                .map(s -> s.getName())
                 .collect(Collectors.toList());
     }
 
-
     //在庫がある果物のみを返す関数
-    public static List<String> inStockFruits() {
-        List<Fruits> newFruitsList = Practice027.fruitsList();
-        return newFruitsList.stream()
+    public static List<Fruits> inStockFruits(List<Fruits> getFruitsList) {
+        return getFruitsList.stream()
                 .filter(s -> s.getStock() != 0)
-                .map(s -> s.getName())
                 .collect(Collectors.toList());
     }
 }
+
+
+//一番単価の高い果物を返す関数【リスト抽出した際】
+//    public static Fruits maxPriceFruits(List<Fruits> getFruitsList) {
+//        List<Fruits> f =  getFruitsList.stream()
+//                    .sorted(comparing(Fruits::getPrice).reversed())
+//                    .collect(Collectors.toList());
+//        return f.get(0);
+//    }
--- src/main/java/practices/Practice029.java
+++ src/main/java/practices/Practice029.java
@@ -18,21 +18,27 @@ import java.util.stream.Collectors;
 public class Practice029 {
     public static void main(String[] args) {
         // ここはご自由にお使いください
-        totalStockPriceMessege();
+        List<Fruits> getMaxFruitsList = Practice027.fruitsList();
+        totalStockPriceMessege(getMaxFruitsList);
     }
 
-    //一番単価が高い果物リスト
-    public static List<Fruits> totalStockPriceList() {
-        List<Fruits> newFruitsList = Practice027.fruitsList();
-        return newFruitsList.stream()
-                .filter(s -> s.getName().equals(Practice028.maxPriceFruits()))
+    //一番単価が高い果物のリスト情報
+    public static List<Fruits> totalStockPriceList(List<Fruits> getMaxFruitsList) {
+        return getMaxFruitsList.stream()
+                .filter(s -> s.getName().matches(Practice028.maxPriceFruits(getMaxFruitsList)))
                 .collect(Collectors.toList());
     }
 
-    public static String totalStockPriceMessege() {
-        int totalStockPrice = totalStockPriceList().get(0).getPrice() * totalStockPriceList().get(0).getStock();
-        System.out.println("世界一" + Practice028.maxPriceFruits() + "（" + totalStockPriceList().get(0).getPrice() + "円）"
-                + totalStockPriceList().get(0).getStock() + "個" + totalStockPrice + "円 お買い上げありがとうございます");
+    public static String totalStockPriceMessege(List<Fruits> getMaxFruitsList) {
+        //一番単価が高い果物の単価
+        int priceOfMaxPriceFruits = totalStockPriceList(getMaxFruitsList).get(0).getPrice();
+        //一番単価が高い果物の在庫数
+        int stockqtyOfMaxPriceFruits = totalStockPriceList(getMaxFruitsList).get(0).getStock();
+        //一番単価が高い果物の在庫金額
+        int totalStockPriceOfMaxPriceFruits = priceOfMaxPriceFruits * stockqtyOfMaxPriceFruits;
+
+        System.out.println("世界一" + Practice028.maxPriceFruits(getMaxFruitsList) + "（" + priceOfMaxPriceFruits + "円）"
+                + stockqtyOfMaxPriceFruits + "個" + totalStockPriceOfMaxPriceFruits + "円 お買い上げありがとうございます");
         return "";
     }
 }
