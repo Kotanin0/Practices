7cc3b1ba6e78c2ce2156d87c1ae91ffeb3df97f1
abi00867 <tatsuru.shimanuki@askul.com>
2019-04-26 14:36:08 +0900

    020の解答の修正

--- src/main/java/practices/Practice020.java
+++ src/main/java/practices/Practice020.java
@@ -8,25 +8,24 @@ import java.util.regex.Pattern;
  * isPhoneNumber
  * isMailAddress
  * をそれぞれ作成しなさい
- *
+ * <p>
  * isPostNumber
  * 入力：文字列
  * 出力：true/false
  * 判定条件：7桁の連続した数値の文字列, 3桁-4桁の数値とハイフンで構成された文字列
  * 例：1234567=true, 123-4567=true, ...
- *
+ * <p>
  * isPhoneNumber
  * 入力：文字列
  * 出力：true/false
  * 判定条件：11桁の連続した数値の文字列, 数値とハイフン2つで構成された文字列
  * 例：09012345678=true, 090-1234-5678=true, 03-1234-5678=true, ...
- *
+ * <p>
  * isMailAddress
  * 入力：文字列
  * 出力：true/false
  * 判定条件：@を必ず含む, 末尾が ".com", ".co.jp", ".ne.jp" で終わる
  * 例：hoge@fuga.com=true, anonymous123@hoge.co.jp=true, ...
- *
  */
 
 public class Practice020 {
@@ -34,14 +33,14 @@ public class Practice020 {
         // ここはご自由にお使いください
         System.out.println(isMailAddress("test@test.com"));
         System.out.println(isMailAddress("test@test.co.jp"));
-        System.out.println(isMailAddress("test@test.ne.jp"));
+        System.out.println(isMailAddress("test@test.nejp"));
         System.out.println(isMailAddress("@test.com"));
         System.out.println(isMailAddress("test@test.co.uk"));
         System.out.println(isMailAddress("090-1234-5678"));
 
     }
 
-       public static boolean isPostNumber(String s) {
+    public static boolean isPostNumber(String s) {
         //k = s.matches("([0-9]{7}|[0-9]{3}-[0-9]{4})&&[^a-z][^A-Z]");
         return s.matches("[0-9]{3}-?[0-9]{4}");
     }
@@ -49,12 +48,12 @@ public class Practice020 {
 
     public static boolean isPhoneNumber(String s) {
 
-        return s.matches("[0-9]{3}-?[0-9]{4}-?[0-9]{4}");
+        return s.matches("[0-9]{2,3}-?[0-9]{4}-?[0-9]{4}");
     }
 
     public static boolean isMailAddress(String s) {
 
-        return  s.matches(".+@.+((\\.com)|(\\.co.jp)|(\\.ne.jp))$");
+        return s.matches(".+@.+((\\.com)|(\\.co\\.jp)|(\\.ne\\.jp))$");
 
 
         //苦しんだあとだわ。
