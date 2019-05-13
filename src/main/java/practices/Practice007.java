7b2a01dc585e99282cf8f9e0702061d5ddcf43b2
takeuchi <ryuta.takeuchi@askul.com>
2019-05-13 15:13:06 +0900

    007リファクタ版です

--- src/main/java/practices/Practice007.java
+++ src/main/java/practices/Practice007.java
@@ -11,13 +11,25 @@ public class Practice007 {
 
     public static void main(String[] args) {
         // ここはご自由にお使いください
-       System.out.println(isPrime(1));
+       System.out.println(isPrime(-2));
 
     }
 
     // 素数判定
     public static boolean isPrime(int n) {
 
+        //一部ガード節にしたが、なんかもう少しいけるような…
+        if(n <= 0 ) throw new IllegalArgumentException("引数の値が不正です");
+        if(n == 1) return false;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+
+        /*
         if(n <= 0 ) {
             throw new IllegalArgumentException("引数の値が不正です");
         }
@@ -32,5 +44,6 @@ public class Practice007 {
             }
             return true;
         }
+        */
     }
 }
