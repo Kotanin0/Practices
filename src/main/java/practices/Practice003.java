d4b5654d5afff8c9c07c030ccf512c7364e67cf0
takeuchi <ryuta.takeuchi@askul.com>
2019-05-13 17:36:22 +0900

    003リファクタ版です※ガード節に変更

--- src/main/java/practices/Practice003.java
+++ src/main/java/practices/Practice003.java
@@ -19,9 +19,20 @@ public class Practice003 {
         // ここはご自由にお使いください
 
     }
-
     // 正の偶数、正の奇数、負の偶数、負の奇数のいずれかを返却する
+
     public static String displayText(int n) {
+
+        //ガード節に変更
+        if (n % 2 == 0 && n >= 0) return "正の偶数";
+        if (n % 2 == 0 && n < 0) return "負の偶数";
+        if (!(n % 2 == 0) && n < 0) return "負の奇数";
+        return "正の奇数";
+
+    }
+
+        /* リファクタ前の記述
+
         if ((n % 2 == 0) && (n >= 0)) {
             return "正の偶数";
         } else if ((n % 2 == 0) && (n < 0)) {
@@ -31,8 +42,9 @@ public class Practice003 {
         } else {
             return "正の奇数";
         }
-
     }
+
+     */
 }
 
 
